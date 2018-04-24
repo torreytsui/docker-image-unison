4a878f34662c15436fd166efc3350a0c0dfad78e
Torrey Tsui <tt@amp.co>
2018-04-24 10:20:23 +0100

    Log progress on initial unison sync

--- precopy_appsync.sh
+++ precopy_appsync.sh
@@ -7,7 +7,7 @@ OWNER_UID=${OWNER_UID:-0}
 if [ ! -f /unison/initial_sync_finished ]; then
 	echo "doing initial sync with unison"
 	# we use ruby due to http://mywiki.wooledge.org/BashFAQ/050
-	time ruby -e '`unison #{ENV["UNISON_ARGS"]} #{ENV["UNISON_SYNC_PREFER"]} #{ENV["UNISON_EXCLUDES"]} -numericids -auto -batch /host_sync /app_sync`'
+	time ruby -e '`unison #{ENV["UNISON_ARGS"]} #{ENV["UNISON_SYNC_PREFER"]} #{ENV["UNISON_EXCLUDES"]} -numericids -auto -batch /host_sync /app_sync -logfile /tmp/unison.log`'
 	#time cp -au  $HOST_VOLUME/.  $APP_VOLUME
 	echo "chown ing file to uid $OWNER_UID"
 	chown -R ${OWNER_UID} ${APP_VOLUME}
